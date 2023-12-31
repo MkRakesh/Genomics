cacd9e24ee2329d88969df8e6af6a0b0a0d27708
MkRakesh <137186397+MkRakesh@users.noreply.github.com>
2023-12-31 15:57:44 +0530

    Update dna.py

--- dna.py
+++ dna.py
@@ -32,7 +32,7 @@ def create_dataframe_column_kmers(df, sequence_column, k):
     return df
 st.text('Created by: Rakesh MK on 31/12/2023')
 with st.expander("Learn More"):
-    st.write("This is additional information you want to provide.")
+    st.write("Trained with Multinominal Naive Bayes Algorithm, This app takes DNA sequence as input and predict the corresponding GENE FAMILY. Enter a valid DNA sequence and explore !")
 
 
 st.title('🧬GENOMIC Classification')
